ac21e5eeec6aa768a1cf5f84442e14bc1b530ecd
Jueun <Jueun@naver.com>
2022-07-18 22:55:41 +0900

    ddd

---
+++ Java Do it/src/chapter2/Frist.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class Frist {
+
+	public static void main(String[] args) {
+         int frist1 = 10;
+         float frist2 = 2.0F;
+         
+         double frist3 = (int) frist1 + (float)2.0F;
+         double frist4 = (float)( frist1 + frist2 );
+         
+         System.out.println(frist3);
+         System.out.println(frist4);
+         
+         
+	}
+
+}
